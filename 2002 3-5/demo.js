9fad9ba096b0adb425e2871aa9951755affcecc6
MIHIRGHADSHI <139873406+MIHIRGHADSHI@users.noreply.github.com>
2023-07-18 17:17:05 +0530

    Add files via upload

---
+++ 2002 3-5/demo.js
@@ -0,0 +1,40 @@
+//document.write("EXTERNAL JAVASCRIPT")  /* multiple line comm */
+/*var num=13;
+document.write(num);
+
+document.write("<br>")
+
+let M=10;
+document.write(M);
+
+document.write("<br>")
+
+const c="ITVedant";
+document.write(c);
+
+var a = prompt("Enter Your Name");
+document.write(a);
+document.write("<br>")
+let b = prompt("Enter Number");
+document.write(b);
+
+function jatin()
+{
+    document.write("HELLO<br>");
+}
+
+
+jatin();
+jatin();*/
+
+function add()
+{
+    var a=prompt("Entr Number 1")
+    a=parseInt(a)
+    var b=prompt("Enter Number 2")
+    b=parseInt(b)
+
+    alert(a-b)
+}
+
+add();
